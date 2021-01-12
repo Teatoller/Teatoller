30fc0f84e384c935da83f20f7a0e709cd96e693b
Teatoller <steven.otieno1@gmail.com>
2021-01-12 16:25:50 +0300

    (refactor): Implement code refactor
    - display hidden about paragraph on small screen.
    - add Github Actions linter
    - fontawesome
    
    [Finishes #008]

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 // Home introduction script
 
-let index = 0;
-let homeTxt = "Hi! I am Steven Ennis and I'm a Software developer.";
-let speed = 100;
+let index = 0
+const homeTxt = "Hi! I am Steven Ennis and I'm a Software developer."
+const speed = 100
 
-const typeWriter=()=> {
+function typeWriter() {
   if (index < homeTxt.length) {
-    document.getElementById("demo").innerHTML += homeTxt.charAt(index);
-    index++;
-    setTimeout(typeWriter, speed);
+    document.getElementById('demo').innerHTML += homeTxt.charAt(index)
+    index++
+    setTimeout(typeWriter, speed)
   }
 }
 
 // Footer date display
 
-document.getElementById("date").innerHTML = new Date().getFullYear();
+document.getElementById('date').innerHTML = new Date().getFullYear()
 
 // loader
 
-let spinner;
+let spinner
 
-const mySpinner=()=> {
-  spinner = setTimeout(showItem, 2000);
+function mySpinner() {
+  spinner = setTimeout(showItem, 2000)
 }
 
-const showItem=()=>{
-  document.getElementById("loader").style.display = "none";
+function showItem() {
+  document.getElementById('loader').style.display = 'none'
 }
